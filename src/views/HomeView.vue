ecc18cc63e101579503099b03fabcb4e4436a5ca
hrosej729 <hrosej729@gmail.com>
2022-06-02 12:59:40 -0500

    added checkboxes visual

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,46 @@
 <template>
   <section class="container">
+    <div>
+      <label for="item-type" class="h2"
+        >Filtered Items: {{ checkedTypes }}</label
+      >
+
+      <br />
+
+      <input type="checkbox" id="books" value="Books" v-model="checkedTypes" />
+      <label for="books"> Books</label>
+
+      <br />
+
+      <input
+        type="checkbox"
+        id="clothes"
+        value="Clothes"
+        v-model="checkedTypes"
+      />
+      <label for="clothes"> Clothes</label>
+
+      <br />
+
+      <input
+        type="checkbox"
+        id="vitamins"
+        value="Vitamins"
+        v-model="checkedTypes"
+      />
+      <label for="vitamins"> Vitamins</label>
+
+      <br />
+
+      <input
+        type="checkbox"
+        id="drinks"
+        value="Drinks"
+        v-model="checkedTypes"
+      />
+      <label for="drinks"> Sports Drinks</label>
+    </div>
+
     <range-selector :products="filteredProducts" v-model="max" />
     <product-list :products="filteredProducts" />
   </section>
